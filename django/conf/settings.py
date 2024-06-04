51100f80939fd7a27400e50963c3a5bd183ce5a0
EC2 Default User <ec2-user@ip-10-1-1-10.us-east-2.compute.internal>
2024-06-04 01:28:51 +0000

    change setting.py

--- django/conf/settings.py
+++ django/conf/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "18.222.11.202"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
